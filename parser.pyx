c9f4d6da45c9ab24601fff07a3ef9f88e2312ca8
Miro Hrončok <miro@hroncok.cz>
2024-07-12 11:41:16 +0200

    Use the Pure Python Cython syntax

--- parser.pyx
+++ parser.pyx
@@ -1,8 +1,9 @@
 # cython: language_level=3
+import cython
 from cython.cimports.libc.stdio import sscanf
 
 
-def parse_line(line: bytes) -> float:
-    cdef float flt
+def parse_line(line: bytes) -> cython.float:
+    flt: cython.float
     sscanf(line, b'%e', &flt)
     return flt
